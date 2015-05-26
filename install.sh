29d62be2eef9d8f1a3993caf10cbbf36b302871e
Nathan Adams <nadams@arbor.net>
2015-05-25 22:59:58 -0400

    Cleaning up install scripts.

--- install.sh
+++ install.sh
@@ -12,20 +12,16 @@ files="gitconfig profile bashrc"      # list of files/folders to symlink in home
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    if [ -f "$file" ]; then
+        mkdir -p $olddir
+        echo "Backing up $file to $olddir"
+        mv ~/.$file $olddir
+    fi
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
